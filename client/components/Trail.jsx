645ce69758df12814ced82ee8e61084cffd209d6
Michael Hodges <michaelcshodges@gmail.com>
2019-08-30 11:54:47 +1200

    racing functionality working

--- client/components/Trail.jsx
+++ client/components/Trail.jsx
@@ -7,23 +7,63 @@ class Trail extends React.Component {
       height: '20px',
       width: '20px'
     },
-    counter: 20
+    style2: {
+      backgroundColor: 'red',
+      height: '20px',
+      width: '20px'
+    },
+    style3: {
+      backgroundColor: 'blue',
+      height: '20px',
+      width: '20px'
+    },
+    counter: 20,
+    counter2: 20,
+    counter3: 20
+  }
+
+  UNSAFE_componentWillMount () {
+    document.addEventListener('keydown', this.buttonHandler.bind(this))
   }
 
   buttonHandler = evt => {
-    this.setState({
-      style: {
-        backgroundColor: 'yellow',
-        height: '20px',
-        width: `${this.state.counter}px`
-      },
-      counter: this.state.counter + 20
-    })
+    if (evt.key === 'q') {
+      this.setState({
+        style: {
+          backgroundColor: 'yellow',
+          height: '20px',
+          width: `${this.state.counter}px`
+        },
+        counter: this.state.counter + 20
+      })
+    } else if (evt.key === 'p') {
+      this.setState({
+        style2: {
+          backgroundColor: 'red',
+          height: '20px',
+          width: `${this.state.counter2}px`
+        },
+        counter2: this.state.counter2 + 20
+      })
+    } else if (evt.key === ' ') {
+      this.setState({
+        style3: {
+          backgroundColor: 'blue',
+          height: '20px',
+          width: `${this.state.counter3}px`
+        },
+        counter3: this.state.counter3 + 20
+      })
+    }
   }
 
   render () {
     return (
-      <div style={this.state.style} onKeyDown={this.buttonHandler} tabIndex = '0'></div>
+      <>
+      <div style={this.state.style} onKeyPress={this.buttonHandler}></div>
+      <div style={this.state.style2} onKeyPress={this.buttonHandler}></div>
+      <div style={this.state.style3} onKeyPress={this.buttonHandler}></div>
+      </>
     )
   }
 }
