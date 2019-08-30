3a838119f062b47010f3508b4535aafc0af6b942
eilish-sinclair <eilishsinclair@gmail.com>
2019-08-30 15:08:21 +1200

    eilish branch

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 
 import Header from './Header'
 import Trail from './Trail'
+import Footer from './Footer'
 
 import FinishLine from './FinishLine';
 
@@ -26,6 +27,7 @@ const App = () => {
       </div>
         <FinishLine />
       <div className='footer'>
+        <Footer />
         <span>Doggo 1</span> <span>Doggo 2</span> <span>Doggo 3</span>
       </div>
     </>
--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,17 +4,35 @@ class Footer extends Component {
   render () {
     return (
       <div>
-        <div className="row">
-          <div className="col-3">
-            Blossom
-          </div>
-          <div className="col-3">
-            ButterCup
-          </div>
-          <div className="col-3">
-            Bubbles
-          </div>
+        <div className="row align-items-center pb-5 justify-content-center teal">
+        <div className="col-3 text-center mx-auto pl-5">
+            <h1>Blossom</h1>
+            <div className="red circle  pr-5"><img src="https://img.icons8.com/color/96/000000/dog.png(3 kB)
+                https://img.icons8.com/color/96/000000/dog.png
+                "/></div>
+
+
         </div>
+        <div className="col-1"></div>
+
+        <div className="col-3 justify-content-center pl-5">
+            <h1 className="text-center pr-5">Bubbles</h1>
+            
+            <div className="green circle"><img src="https://img.icons8.com/color/96/000000/yorkshire-terrier.png(3 kB)
+                https://img.icons8.com/color/96/000000/yorkshire-terrier.png
+                " /></div>
+
+        </div>
+        <div className="col-1"></div>
+        <div className="col-3 text-center mx-auto pl-5">
+            <h1 >Buttercup</h1>
+            <div className="aqua circle text-center pr-5"><img src="https://img.icons8.com/color/96/000000/german-shepherd.png"/></div>
+
+        </div>
+
+
+        </div> 
+    
       </div>
     )
   }
