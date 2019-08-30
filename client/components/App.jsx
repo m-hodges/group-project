267e0e35abef7594a202918535cb4b3c12740eba
James <james_wylie@outlook.com>
2019-08-30 12:09:35 +1200

    background start

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Dog from './Dog'
+
+
+
+
 
 const App = () => {
   return (
+    <div className="div">
+     <Route exact path="/" component={Dog} />
     <h1>React development has begun!</h1>
+    </div>
   )
 }
 
---
+++ client/components/Dog.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Dog extends React.Component {
+  render() {
+    return (
+    
+    )
+  }
+}
+
+export default Dog
